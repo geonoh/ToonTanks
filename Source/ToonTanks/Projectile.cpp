f4ba74a082fe6ac9e0c1217baec1729bc442f3da
Geon Oh <ohhh123@naver.com>
2025-08-10 21:03:10 +0900

    CameraShake added
    - Projectile shooting shaking
    
    - Pawn death shaking

--- Source/ToonTanks/Projectile.cpp
+++ Source/ToonTanks/Projectile.cpp
@@ -72,6 +72,11 @@ void AProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimi
 		{
 			UGameplayStatics::PlaySoundAtLocation(this, HitSound, GetActorLocation());
 		}
+
+		if (HitCameraShakeClass)
+		{
+			GetWorld()->GetFirstPlayerController()->ClientStartCameraShake(HitCameraShakeClass);
+		}
 	}
 
 	Destroy();
